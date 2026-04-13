7223fba0f74a75a6e9b67387e56dce29da672192
thedeveljun <thedeveljun@gmail.com>
2026-04-13 23:56:22 +0900

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-/* Minimal service worker so the app can be installed as a PWA (Chrome/Android). */
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
