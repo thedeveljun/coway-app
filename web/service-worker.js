da52588759cabb00b91932c6e33075ef3aef05ec
thedeveljun <thedeveljun@gmail.com>
2026-04-15 13:36:59 +0900

    Update service-worker.js

--- web/service-worker.js
+++ web/service-worker.js
@@ -1,8 +1,34 @@
-/* Minimal service worker so the app can be installed as a PWA (Chrome/Android). */
-self.addEventListener('install', (event) => {
+const CACHE_NAME = 'coway-cache-v3';
+const urlsToCache = [
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json',
+  './favicon.png',
+  './icons/Icon-192.png',
+  './icons/Icon-512.png',
+  './icons/Icon-maskable-192.png',
+  './icons/Icon-maskable-512.png'
+];
+
+self.addEventListener('install', event => {
   self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
